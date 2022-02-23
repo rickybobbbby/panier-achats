2b17d1064ccd5a34559345e7ac6db1afc5e49a1c
rickybobbbby <yannicklanglois99@gmail.com>
2022-02-23 14:53:20 -0500

    commit fin cours 1 sem 5

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -4,12 +4,13 @@ import Badge from '@mui/material/Badge';
 
 export default function Entete(props) {
     console.log("Les props du composant Entête : ", props);
-    console.log("Le panier dans Entête : ", props.panier);
+    console.log("Le panier dans Entête : ", Object.values(props.panier));
+    //let totalQte = Object.values(props.panier).reduce((acc, article) => acc+article.qte, 0);
     return (
         <header className="Entete">
             <h1>Magasin général</h1>
             <nav>
-                <Badge badgeContent={Object.values(props.panier).length} color="secondary">
+                <Badge badgeContent={Object.values(props.panier).reduce((acc, article) => acc+article.qte, 0)} color="secondary">
                     <ShoppingCartSharpIcon/>
                 </Badge>
                 <a href="#">Contactez-nous</a>
